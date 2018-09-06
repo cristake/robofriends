228192fb08693b31c3a54b242ebb038ffcf0304f
Cristian Iosif <cristianiosif@me.com>
2018-09-07 00:27:02 +0300

    adds redux

---
+++ src/containers/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
+import Scroll from '../components/Scroll'
+import ErrorBoundry from '../components/ErrorBoundry'
+import './App.css'
+
+import { setSearchField } from '../actions'
+
+const mapStateToProps = (state) => ({ searchField: state.searchField })
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	}
+}
+
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			robots: []
+		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => this.setState({ robots: users}))
+	}
+
+	render () {
+		const { robots } = this.state
+		const { searchField, onSearchChange } = this.props
+
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchField.toLowerCase());
+		})
+
+		if( !robots.length ) {
+			return <h1 className="tc">Loading...</h1>
+		} else {
+			return (
+				<div className="tc">
+					<h1 className="f1">RoboFriends</h1>
+					<SearchBox searchChange={ onSearchChange } />
+					<Scroll>
+						<ErrorBoundry>
+							<CardList robots={ filteredRobots } />
+						</ErrorBoundry>
+					</Scroll>
+				</div>
+			);
+		}
+	};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux'
+import { createStore } from 'redux'
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import { searchRobots } from './reducers';
+import './index.css';
+import 'tachyons';
+
+const store = createStore(searchRobots)
+
+ReactDOM.render(
+	<Provider store = { store }>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
+registerServiceWorker();
